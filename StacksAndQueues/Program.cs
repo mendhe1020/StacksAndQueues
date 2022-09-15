55f382615916d848ee36c4f304a5f1d1688c0fa6
mendhe1020 <anurag.mendhe14@gmail.com>
2022-09-15 19:12:52 +0530

    [Anurag] Add: Create A Status

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Stacks And Queues");
+
+            StackAndQueues list = new StackAndQueues();
+
+            list.Push(56);
+            list.Push(30);
+            list.Push(70);
+
+            list.Display();
+
         }
     }
 }
